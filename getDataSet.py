9d42a5c74dcba8cfff63d228588fe2bc32f5a709
rowenci <459287499@qq.com>
2021-08-21 17:11:00 +0800

    Update getDataSet.py

--- getDataSet.py
+++ getDataSet.py
@@ -1,5 +1,3 @@
-from PIL import Image
-from torchvision import transforms
 import logConfig # logging
 import numpy as np
 import pandas as pd
@@ -8,39 +6,11 @@ import torch
 
 logger = logConfig.getLogger("logs/imageProcessing.log")
 
-def loadTrainImages():
-    # image list
-    images = np.ndarray((45000, 3, 32, 32), dtype=np.float32)
-    while True:
-        i = 0
-        try:
-            # open a image
-            imageLabel = i + 1
-            img_path = "datas/train/" + str(imageLabel) + ".png"
-            img = Image.open(img_path)
-        except FileNotFoundError: # 没有该图片或者图片读取完成
-            if i == 45001:
-                logger.info("image loader finished")
-            else:
-                logger.error("image not found", img_path)
-            break
-        else:
-            # transfer image type into numpy
-            img = np.array(img, dtype=np.float32)
-            img.resize(3, 32, 32)
-            images[i, :, :, :] = img
-            i += 1
-    return images
-
-def loadTrainLabels():
-    labels = pd.read_csv("datas/trainLabels.csv")
-    labels = np.array(labels["label"])
-    return labels[:45001]
 
 class TrainDataSet(torch.utils.data.Dataset):
     def __init__(self):
-        images = loadTrainImages()
-        labels = loadTrainLabels()
+        images = np.load("datas/pixeled_train/images.npy", allow_pickle=True)
+        labels = np.load("datas/pixeled_train/labels.npy", allow_pickle=True)
 
         self.images = images
         self.labels = labels
@@ -53,39 +23,10 @@ class TrainDataSet(torch.utils.data.Dataset):
         return data
 
 
-def loadTestImages():
-    # image list
-    images = np.ndarray((5000, 3, 32, 32), dtype=np.float32)
-    while True:
-        i = 0
-        try:
-            # open a image
-            imageLabel = i + 1
-            img_path = "datas/train/" + str(imageLabel) + ".png"
-            img = Image.open(img_path)
-        except FileNotFoundError: # 没有该图片或者图片读取完成
-            if i == 50001:
-                logger.info("image loader finished")
-            else:
-                logger.error("image not found", img_path)
-            break
-        else:
-            # transfer image type into numpy
-            img = np.array(img, dtype=np.float32)
-            img.resize(3, 32, 32)
-            images[i, :, :, :] = img
-            i += 1
-    return images
-
-def loadTestLabels():
-    labels = pd.read_csv("datas/trainLabels.csv")
-    labels = np.array(labels["label"])
-    return labels[45001:]
-
 class TestDataSet(torch.utils.data.Dataset):
     def __init__(self):
-        images = loadTestImages()
-        labels = loadTestLabels()
+        images = np.load("datas/pixeled_test/images.npy", allow_pickle=True)
+        labels = np.load("datas/pixeled_test/labels.npy", allow_pickle=True)
 
         self.images = images
         self.labels = labels
