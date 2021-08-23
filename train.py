5dd3baa868f0bf18db46d83c6f80c92621780231
rowenci <459287499@qq.com>
2021-08-23 15:20:33 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import model.ResNet
 import getDataset
 
 batch_size = 10
+lr = 1e-3
+epochs = 100
 
 train_dataset = getDataset.TrainDataSet()
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
@@ -11,13 +14,14 @@ train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True
 test_dataset = getDataset.TestDataSet()
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-"""
-for data in train_dataloader:
-    imgs, labels = data
-    print(imgs)
-    print(labels)
-    print(imgs.shape)
-    print(labels.shape)
-    break
-"""
+net = model.ResNet.getResNet()
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+criterion = nn.CrossEntropyLoss()
 
+net.train()
+for epoch in epochs:
+    # train
+    for data in test_dataloader:
+        imgs, labels = data
+        
+        break
